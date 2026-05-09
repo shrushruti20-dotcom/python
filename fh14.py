3938df73edba100b8fcd1e584c813f40ead1e53e
shrushruti20-dotcom <shrushruti2.0@gmail.com>
2026-05-09 20:50:46 +0530

    Update fh14.py

--- fh14.py
+++ fh14.py
@@ -11,15 +11,5 @@ my_file.write("Hi iam shruti")
 my_file.close()
 os.remove('sample.txt')
 os.randir('folder')
-#duplicating
-utputfile = open('updatesfile.txt','w')
-inputfile = open('repeated.txt','r')
-lines_seen_so_far = set()
-print('eliminating duplicate lines..')
-for line in inputfile:
-    if line not in lines_seen_so_far:
-        outputfile.write(line)
-        lines_seen_so_far.add(line)
-inputfile.close()
-outputfile.close()
+
 
